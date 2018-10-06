90deeb4a665823d1bc164b2284d8e27180bbaf65
ChrisGarcia <chrisg905@gmail.com>
2018-10-06 14:58:34 -0500

    This change takes the logs.txt file created and gets rid of all the lines less than 38 characters. This should get rid of any incomplete data lines.

--- script.py
+++ script.py
@@ -1,9 +1,26 @@
-try:
-    import urllib.request as urllib2
-except (ImportError):
-    import urllib2
+# This script was made by Colton McAda and Chris Garcia
 
-url='https://s3.amazonaws.com/tcmg476/http_access_log'
-response=urllib2.urlopen(url)
-with open('logs.txt','wb') as f:
-    f.write(response.read())
+# This is function pulls the logs from the website and saves into a file.
+def savelogs():
+    try:
+        import urllib.request as urllib2
+    except (ImportError):
+        import urllib2
+
+    url='https://s3.amazonaws.com/tcmg476/http_access_log'
+    response=urllib2.urlopen(url)
+    
+    with open('logs.txt','wb') as f:
+        f.write(response.read())
+
+savelogs()
+
+# This function will take that log file and delete any bad lines
+def clean_logs():
+    with open("cleanlogs.txt", "w") as out:
+        with open("logs.txt") as f:
+            for line_no, line in enumerate(f):
+                if len(line.strip()) > 38:
+                    out.write(line)
+
+clean_logs()
